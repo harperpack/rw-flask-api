66442690996913d9a8c1c52f247bc92c718ad5f9
Harper <charlespack2019@u.northwestern.edu>
2023-02-24 12:37:09 -0600

    local use instructions

--- local_dev.sh
+++ local_dev.sh
@@ -4,10 +4,11 @@ python -m venv venv
 source venv/bin/activate
 # pip install wheel
 pip install flask
+pip install flask_cors
 # pip install gunicorn flask
-export FLASK_APP=app
+export FLASK_APP=./core/app
 # export FLASK_ENV=development
-# export FLASK_DEBUG=true
+export FLASK_DEBUG=true
 # sudo ufw allow 5000
 # gunicorn --bind 0.0.0.0:5000 wsgi:app
-flask run --debug
+flask run
