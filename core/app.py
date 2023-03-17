7c76690103e3292583b7636f7f5afec2535a4987
Harper <charlespack2019@u.northwestern.edu>
2023-03-17 11:06:49 -0500

    authorization, seemingly no cors issues

--- core/app.py
+++ core/app.py
@@ -10,6 +10,8 @@ import json
 import datetime
 from traceback import print_exc
 
+import time
+
 load_dotenv()
 app = Flask(__name__)
 
@@ -17,16 +19,18 @@ FILE_PREFIX = 'files'
 FILE_SUFFIX = '.json'
 VERBOSE = True
 
-## TODO
-# > Ensure always wrapped in try/except (presumably cause of CORS outage?)
-# > Strip file extensions? / remove spaces in file names?
-# > Add /status endpoint?
-# > Clean up file post disaster
-# > 
-
 app.config['PERMISSION_KEY'] = os.environ.get('PERMISSION_KEY')
 app.config['PERMISSION_VAL_KEY'] = os.environ.get('PERMISSION_VAL_KEY')
 app.config['PERMISSION_VAL'] = os.environ.get('PERMISSION_VAL')
+
+## TODO
+# > Add API keys / authentication
+# > 
+# > Ensure always wrapped in try/except (presumably cause of CORS outage?)
+# > Strip file extensions?
+# > Add /status endpoint?
+# > Clean up file post disaster
+# > 
 
 def permission_required(fnctn):
     @wraps(fnctn)
@@ -54,8 +58,27 @@ def get_content(file_path, clear=False):
         #     print("Getting {}".format(file_path))
         if not os.path.exists(file_path): 
             return jsonify({"content":"Read Error | File path does not exist."}), 404
-        with open(file_path, 'r', encoding='utf-8') as f:
-            contents = json.load(f)
+        try:
+            with open(file_path, 'r', encoding="UTF-8") as f: #, encoding='utf-8') as f:
+                contents = json.load(f)
+        except:
+            time.sleep(0.5)
+            try:
+                with open(file_path, 'r', encoding='UTF-8', errors='ignore') as f:
+                    contents = json.load(f)
+            except:
+                time.sleep(0.5)
+                try:
+                    with open(file_path, 'r', errors='ignore') as f:
+                        contents = json.load(f)
+                except:
+                    time.sleep(0.5)
+                    try:
+                        with open(file_path, 'r', encoding='UTF-8', errors='ignore') as f:
+                            contents = json.loads(f.read())
+                    except:
+                        with open(file_path, 'r', encoding='UTF-8', errors='ignore') as f:
+                            contents = json.load(f) 
         if clear:
             set_contents(file_path, [], allow_blank=True)
         return jsonify(contents), 200
@@ -73,7 +96,7 @@ def set_contents(file_path, contents, allow_blank=False):
         elif not allow_blank and not contents[0]:
             return jsonify({"content": "Write Error | No write content specified."}), 400
         json_to_write = {"content": contents}
-        with open(file_path, 'w', encoding='utf-8') as f:
+        with open(file_path, 'w', encoding='UTF-8') as f:
             # if VERBOSE:
             #     print("Setting {} to {}".format(json_to_write, file_path))
             json.dump(json_to_write, f, ensure_ascii=False, indent=4)
@@ -89,34 +112,7 @@ def set_contents(file_path, contents, allow_blank=False):
 def get_path(file):
     return os.path.join(FILE_PREFIX, file+FILE_SUFFIX)
     
-routes = {"endpoints": ['/help', '/write', '/append', '/read', '/latest', '/clear']}
-# routes = {
-#     "endpoints":{
-#         "GET":{
-#             "/api/help/":{
-#                 "doc": "List the API endpoints.",
-#                 "in": "n/a",
-#                 "out": "JSON object with methods, explanations, and i/o for each API endpoint."
-#             },
-#         },
-#         "POST":{
-#             "/api/read/":{
-#                 "doc": "Read the content of a specified file.",
-#                 "in": "JSON object with key 'file' whose value is a string (the name of the file to be read).",
-#                 "out": "JSON object with key 'content' whose value is a list of JSON content objects written to that specified file, or a String containing an error message.",
-#             },
-#             "/api/write/":{
-#                 "doc": "Write the specified content to a specified file.",
-#                 "in": "JSON object with key 'file', whose value is a sting (the name of the file to be written), and key 'content', whose value is the JSON object to be written to that file.",
-#                 "out": "JSON object with key 'content' whose value is a String indicating the write operation was a success, or a String containing an error message.",
-#             },
-#         },
-#     },
-#     "header_parameters":{
-#         "file": "File to be read from or written to, without extension. API uses parameter to write to: files/{$file}.json",
-#         "content": "JSON object to be written to the specified file. File at files/<$file>.json will subsequently look like: {'content': {'time':<$timestamp>,<$other key-value pairs of JSON object found at key 'content' of header>},}",
-#     },
-# }
+routes = {"endpoints": ["/help", "/write", "/append", "/read", "/latest", "/clear"]}
 
 @app.route('/')
 @cross_origin()
@@ -125,7 +121,6 @@ def hello():
 
 @app.route('/api/help/', methods=['GET'])
 @cross_origin()
-@permission_required
 def list_content():
     if VERBOSE:
         print("Available endpoints.")
@@ -136,8 +131,8 @@ def list_content():
 @permission_required
 def read_content():
     input = request.json
-    # if VERBOSE:
-    #     print("Reading content of {}".format(input["file"]))
+    if VERBOSE:
+        print("Reading content of {}".format(input["file"]))
     return get_content(get_path(input["file"]))
 
 @app.route('/api/latest/', methods=['POST'])
@@ -150,10 +145,26 @@ def latest_content():
     write_path = get_path(input["file"])
     response = get_content(write_path)
     read_content = response[0].json
-    write_content = read_content.get('content', [])
+    write_content = read_content.get("content", [])
     if not write_content:
         return jsonify({"content": []}), 200
-    return_content = write_content.pop(0)
+    if isinstance(write_content, list):
+        return_content = write_content.pop(0)
+    elif isinstance(write_content, str):
+        return_content = write_content
+        write_content = []
+    else:
+        if write_content.get("content", []) and isinstance(write_content.get("content", []), list):
+            return_content = write_content.get("content").pop(0)
+            write_content = write_content.get("content")
+        elif write_content.get("content", []):
+            return_content = write_content.get("content")
+            write_content = []
+        else:
+            return_content = write_content
+            write_content = []
+    if VERBOSE:
+        print("Updating {} to {}...".format(input["file"], write_content))
     set_contents(write_path, write_content, allow_blank=True)
     return jsonify({"content": return_content}), 200
 
@@ -173,7 +184,7 @@ def clear_content():
 def write_content():
     input = request.json
     # ASSUME ALL WRITE CONTENT IS A JSON OBJECT
-    new_content = input.get('content', {})
+    new_content = input.get("content", {})
     # ASSUME CONTENT IS ALWAYS A LIST OF JSON OBJECTS
     write_content = [new_content]
     write_path = get_path(input["file"])
@@ -188,14 +199,17 @@ def append_content():
     try:
         input = request.json
         # ASSUME ALL APPEND CONTENT IS A JSON OBJECT
-        new_content = input.get('content', {})
+        new_content = input.get("content", {})
         write_path = get_path(input["file"])
         response = get_content(write_path)
         if response[1] == 200:
             read_content = response[0].json
-            write_content = read_content.get('content', [])
-            # IS WRITE_CONTENT NOW A LIST? IT MUST BE
+            write_content = read_content.get("content", [])
+            # IS WRITE_CONTENT NOW A LIST? MUST NOT BE
             while not isinstance(write_content, list):
+                if isinstance(write_content, str):
+                    write_content = [write_content]
+                    break
                 write_content = write_content.get("content", [])
             # ASSUME CONTENT IS ALWAYS A LIST OF JSON OBJECTS
             write_content.append(new_content)
@@ -205,7 +219,7 @@ def append_content():
             # get_content() SHOULD ONLY RETURN 200 OR 404
             raise ValueError(f'{response[0].json}')
         if VERBOSE:
-            print("Writing {} to {}.".format(write_content, input["file"]))
+            print("Appending {} to {}.".format(write_content, input["file"]))
         return set_contents(write_path, write_content)
     except Exception as e:
         if VERBOSE:
